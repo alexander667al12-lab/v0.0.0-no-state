453a128fa612987598ca24ff9ad006855f01d0fa
alexander667al12-lab <alexander667al12@gmail.com>
2026-05-29 01:17:04 +0300

    Delete gamemode.js

--- gamemode.js
+++
@@ -1,5 +0,0 @@
-import * as room from "pixel_combats/room"
-room.Players.OnPlayerConnected.Add(function(p){}
-room.Teams.OnRequestJoinTeam.Add(function(p.t){ t.Add(p) })
-room.Teams.OnPlayerChangeTeam.Add(())
-room.Teams.Add("Red","красные",new basic.Color(1,0,0,1)
