63ee1761c7c400dfb1eabeb5bf186e7d7afebdc0
alexander667al12-lab <alexander667al12@gmail.com>
2026-05-29 19:52:06 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,5 +1,5 @@
 import * as room from "pixel_combats/room"
 room.Players.OnPlayerConnected.Add(function(p){})
 room.Teams.OnRequestJoinTeam.Add(function(p.t){ t.Add(p) })
-room.Teams.OnPlayerChangeTeam.Add(((function(p, t) { p Spawns.Spawn(); })
+room.Teams.OnPlayerChangeTeam.Add(function(p, t) { p.Spawns.Spawn(); });
 room.Teams.Add("Red","красные",new basic.Color(1,0,0,1)
